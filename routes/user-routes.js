b51915bbe5c8884a72af8492218d8a1f72e2d080
RahulS <rupash8754@gmail.com>
2023-10-21 19:48:26 +0530

    part-2

---
+++ routes/user-routes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import {getAllUsers} from "../controller/user-controller";
+
+const userRouter=express.Router();
+userRouter.get("/",getAllUsers);
+
+export default userRouter
