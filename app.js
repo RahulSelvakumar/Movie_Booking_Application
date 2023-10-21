6e82b44e42555f40d45006d3bb3ae9d0ce3a6ab4
RahulS <rupash8754@gmail.com>
2023-10-21 19:40:32 +0530

    part-2

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv=require("dotenv");
+const app = express();
+
+app.use("/users",userRouter);
+mongoose
+  .connect(
+    `mongodb+srv://test:${process.MONGOOSE_PASSWORD}@cluster0.4pek4xr.mongodb.net/`
+  )
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("listening to port 3000 http://localhost:3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
