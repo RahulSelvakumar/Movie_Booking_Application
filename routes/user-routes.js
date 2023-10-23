38f0ac03981c7299f492b319424d170f52942392
RahulS <rupash8754@gmail.com>
2023-10-23 09:45:23 +0530

    backend completed

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,11 +1,14 @@
-const express=require("express")
-import { getAllUser, signUp, updateUser, deleteUser, logIn} from "../controller/user-controller";
+const express = require('express');
+const userRouter = express.Router();
 
-const userRouter=express.Router();
-userRouter.get("/",getAllUser);
-userRouter.post("/singup",signUp);
+const { getAllUser, signUp, updateUser, deleteUser, logIn, getBookingofUser, getUserById } = require('../controllers/user-controller');
+
+userRouter.get("/", getAllUser);
+userRouter.post("/signup", signUp)
 userRouter.put("/:id", updateUser)
 userRouter.delete("/:id", deleteUser)
 userRouter.post("/login", logIn)
+userRouter.get("/bookings/:id", getBookingofUser);
+userRouter.get("/:id", getUserById);
 
-export default userRouter
+module.exports = userRouter 
